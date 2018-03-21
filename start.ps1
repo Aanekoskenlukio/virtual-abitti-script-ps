afd7de8b12b05128e2cdab195e3f98e42685205c
Sami Jakonen <sammy@live.fi>
2018-03-21 10:42:10 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,3 +1,28 @@
-cd ~
-cd ktp
+# Sami Jakonen - jakonen@GitHub
+# Käynnistää virtuaalisen Abitti-palvelimen automatisoidusti
+$env:HOMEDRIVE="C:"
+$env:HOMEPATH="\Users\" + $Env:USERNAME + "\"
+
+$FULLPATH = $env:HOMEDRIVE + $env:HOMEPATH + "ktp"
+echo ""
+echo "Poluksi valittu:"
+echo $FULLPATH
+echo ""
+echo ""
+
+cd $FULLPATH
+
+echo ""
+echo ""
+echo "Etsitään kokeita hakemistosta:"
+echo "ktp-jako"
+echo ""
+echo ""
+
+echo "------- [ PALVELIMELLA OLEVAT KOKEET ] -------\n\n"
+
+Get-ChildItem -Path ..\ktp-jako
+echo ""
+echo "Varmista, että koe on listattu yllä. Jatketaan automaattisesti..."
+sleep(6)
 vagrant up
